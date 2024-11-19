ba8708f185c3621a86889be04b08ef6467b44b36
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2024-11-19 21:47:01 +0100

    fixed sities overwriting style

--- scripts.js
+++ scripts.js
@@ -6,16 +6,8 @@ chrome.storage.sync.get({blockedSites: []},(data) => {
     listOfBlockedSites.forEach(siteDomain => {
         if(siteDomain === window.location.hostname){
 
-            fetch(chrome.runtime.getURL("blocked/blocked.html"))
-                .then(response => response.text())
-                .then(html => {
-                    document.open();
-                    document.write(html);
-                    document.close();
-                })
-                .catch(error => console.error("Error loading blocked page:", error));
+            window.location.href = window.location.href = chrome.runtime.getURL("blocked/blocked.html");
         }
     });
 
 });
-
